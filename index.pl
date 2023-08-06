920f73063153491d0378968f5483b880e61dda46
Gonzalo Martin Ramirez <86808851+gonzaramireez1188@users.noreply.github.com>
2023-08-06 13:46:11 -0300

    Punto 2, 3 y 4

--- index.pl
+++ index.pl
@@ -3,31 +3,40 @@ cucaracha(erikElRojo,25,70).
 cucaracha(gimeno,12,8).
 cucaracha(cucurucha,12,5).
 
-comio(pumba,vaquitaSanAntonio(gervasia,3)). 
-comio(pumba,hormiga(federica)). 
+comio(pumba, vaquitaSanAntonio(gervasia,3)). 
+comio(pumba, hormiga(federica)). 
 
-comio(pumba,hormiga(tuNoEresLaReina)). 
-comio(pumba,cucaracha(ginger,15,6)). 
-comio(pumba,cucaracha(erikElRojo,25,70)). 
+comio(pumba, hormiga(tuNoEresLaReina)). 
+comio(pumba, cucaracha(ginger,15,6)). 
+comio(pumba, cucaracha(erikElRojo,25,70)). 
 
-comio(timon,vaquitaSanAntonio(romualda,4)). 
-comio(timon,cucaracha(gimeno,12,8)). 
-comio(timon,cucaracha(cucurucha,12,5)). 
+comio(timon, vaquitaSanAntonio(romualda,4)). 
+comio(timon, cucaracha(gimeno,12,8)). 
+comio(timon, cucaracha(cucurucha,12,5)). 
 
-comio(simba,vaquitaSanAntonio(remeditos,4)). 
-comio(simba,hormiga(schwartzenegger)). 
-comio(simba,hormiga(niato)). 
-comio(simba,hormiga(lula)). 
+comio(simba, vaquitaSanAntonio(remeditos,4)). 
+comio(simba, hormiga(schwartzenegger)). 
+comio(simba, hormiga(niato)). 
+comio(simba, hormiga(lula)). 
+
+comio(shenzi,hormiga(conCaraDeSimba)). 
 
 pesoHormiga(2). 
 
-peso(pumba,100). 
-peso(timon,50). 
-peso(simba,200). 
+peso(pumba, 100). 
+peso(timon, 50). 
+peso(simba, 200). 
+peso(scar,300). 
+peso(shenzi,400). 
+peso(banzai,500). 
 
 esPersonaje(simba).
 esPersonaje(pumba).
 esPersonaje(timon).
+esPersonaje(scar).
+esPersonaje(shenzi).
+esPersonaje(banzai).
+esPersonaje(mufasa).
 
 ## 1
 # a
